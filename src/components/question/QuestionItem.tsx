2b63af7916207c93e1d5f6b5fb70bba06965459a
tolu-c <tolu.adeyemo7@gmail.com>
2023-04-10 12:55:57 +0100

    more answers fetching

--- src/components/question/QuestionItem.tsx
+++ src/components/question/QuestionItem.tsx
@@ -1,11 +1,39 @@
-import { Question } from "../../utils/interfaces";
+import { useEffect } from "react";
+import useQuiz from "../../hooks/useQuiz";
+import { Question, Answer } from "../../utils/interfaces";
+import AnswerItem from "../answers/AnswerItem";
 
 type QuestionItemProps = {
   question: Question;
 };
 
 const QuestionItem = ({ question }: QuestionItemProps) => {
-  return <li>{question.questionText}</li>;
+  const { handleGetAnswer, setShowLoader, answers } = useQuiz();
+  const fetchAnswer = () => {
+    handleGetAnswer(question.questionID).finally(() => {
+      setShowLoader(false);
+    });
+  };
+
+  useEffect(() => {
+    fetchAnswer();
+    //eslint-disable-next-line
+  }, []);
+
+  return (
+    <li>
+      {question.questionText}
+      {!answers || answers.length === 0 ? (
+        <p>No answers yet</p>
+      ) : (
+        <ul>
+          {answers.map((answer: Answer) => (
+            <AnswerItem key={answer.answerID} answer={answer} />
+          ))}
+        </ul>
+      )}
+    </li>
+  );
 };
 
 export default QuestionItem;
